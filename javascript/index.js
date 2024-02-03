12907cbb1c44aabf653a402dc946f15a06c563df
heatherleeriley <heatherleeriley@yahoo.com>
2024-02-02 21:35:25 -0500

    added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,23 +1,60 @@
 function updateTime() {
-  let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  let thessalonikiElement = document.querySelector("#thessaloniki");
+  if (thessalonikiElement) {
+    let thessalonikiDateElement = thessalonikiElement.querySelector(".date");
+    let thessalonikiTimeElement = thessalonikiElement.querySelector(".time");
+    let thessalonikiTime = moment().tz("Europe/Athens");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    thessalonikiDateElement.innerHTML = thessalonikiTime.format("MMMM Do YYYY");
+    thessalonikiTimeElement.innerHTML = thessalonikiTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-  let sydneyElement = document.querySelector("#sydney");
-  let sydneyDateElement = sydneyElement.querySelector(".date");
-  let sydneyTimeElement = sydneyElement.querySelector(".time");
-  let sydneyTime = moment().tz("Australia/Sydney");
+  let lisbonElement = document.querySelector("#lisbon");
+  if (lisbonElement) {
+    let lisbonDateElement = lisbonElement.querySelector(".date");
+    let lisbonTimeElement = lisbonElement.querySelector(".time");
+    let lisbonTime = moment().tz("Europe/Lisbon");
 
-  sydneyDateElement.innerHTML = sydneyTime.format("MMMM Do YYYY");
-  sydneyTimeElement.innerHTML = sydneyTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    lisbonDateElement.innerHTML = lisbonTime.format("MMMM Do YYYY");
+    lisbonTimeElement.innerHTML = lisbonTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  let saugatuckElement = document.querySelector("#saugatuck");
+  if (saugatuckElement) {
+    let saugatuckDateElement = saugatuckElement.querySelector(".date");
+    let saugatuckTimeElement = saugatuckElement.querySelector(".time");
+    let saugatuckTime = moment().tz("America/New_York");
+
+    saugatuckDateElement.innerHTML = saugatuckTime.format("MMMM Do YYYY");
+    saugatuckTimeElement.innerHTML = saugatuckTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss"
+          )} <small>${cityTime.format("A")}</small></div>
+        </div>
+  `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
